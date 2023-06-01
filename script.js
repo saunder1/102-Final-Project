9f280acd96473b457bfd50bce766741509ee35f5
Saunder VanWoerden <saunder@Saunders-MacBook-Pro.local>
2023-06-01 13:45:28 -0700

    working on nav bar, added parallax

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const tl = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#parallax",
+		start: "top top",
+		end: "bottom top",
+		scrub: 0.5
+	}
+});
 
+gsap.utils.toArray(".parallax").forEach(layer => {
+	const depth = layer.dataset.depth;
+	const movement = (layer.offsetHeight * depth)
+	tl.to(layer, {y: movement, ease: "none"}, 0)
+});
+
+// window.addEventListener('scroll', function() {
+//     var header = document.querySelector('.parallax');
+//     var scrollY = window.scrollY;
+//     header.style.top = scrollY / 4 + 'px';
+// });
